08790fd4f786a9811eaa1f39836c726b486bcefc
mooxiu <monetshaw@outlook.com>
2022-06-04 12:14:01 +0800

    update

---
+++ start_me_up.sh
@@ -0,0 +1,15 @@
+# Pay Attention! 
+# this script is for mac only!
+
+# install some important software
+# install homebrew, see: https://brew.sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# iterm
+brew install --cask iterm2
+# zsh 
+brew install zsh
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
